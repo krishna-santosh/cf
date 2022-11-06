40426d87c9144be3a9be3804da9be0eafa53c46b
Krishna Santosh <75202541+krishna-santosh@users.noreply.github.com>
2022-11-06 18:57:44 +0530

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,106 @@
+use std::{env, num::ParseFloatError, process::exit};
+
+enum Unit {
+    Celsius,
+    Fahrenheit,
+}
+
+fn main() {
+    let (choice, temperature) = parse_args();
+
+    match choice.as_str() {
+        "-f" => {
+            println!(
+                "{}° Fahrenheit = {:.2}° Celsius",
+                temperature,
+                convert(temperature, Unit::Fahrenheit)
+            );
+        }
+
+        "-c" => {
+            println!(
+                "{}° Celsius = {:.2}° Fahrenheit",
+                temperature,
+                convert(temperature, Unit::Celsius)
+            );
+        }
+        _ => (),
+    }
+}
+
+const VERSION: &'static str = env!("CARGO_PKG_VERSION");
+
+const HELP_MENU: &str = "
+Celsius <=> Fahrenheit
+
+Usage:
+    cf <flag> <temperature>
+
+    flags:
+        -f | --fahrenheit      To convert Fahrenheit to Celsius
+        -c | --celsius         To convert Celsius to Fahrenheit
+        -v | --version         Version
+        -h | --help            Help Menu
+
+Example:
+    cf -f 100
+    cf -c 37.78";
+
+fn parse_args() -> (String, f64) {
+    let args: Vec<String> = env::args().collect();
+
+    let args_len = args.len();
+
+    if args_len > 3 || args_len == 1 {
+        println!("{}", HELP_MENU);
+        exit(1);
+    }
+
+    match args[1].as_str() {
+        "-f" | "--fahrenheit" => {
+            if args_len != 3 {
+                println!("{}", HELP_MENU);
+                exit(1)
+            }
+            return ("-f".to_string(), parse_float(&args[2]));
+        }
+        "-c" | "--celsius" => {
+            if args_len != 3 {
+                println!("{}", HELP_MENU);
+                exit(1)
+            }
+            return ("-c".to_string(), parse_float(&args[2]));
+        }
+        "-h" | "--help" => {
+            println!("{}", HELP_MENU);
+            exit(0)
+        }
+        "-v" | "-V" | "--version" => {
+            println!("cf v{}", VERSION);
+            exit(0)
+        }
+        _ => {
+            println!("{}", HELP_MENU);
+            exit(1);
+        }
+    }
+}
+
+fn parse_float(arg: &String) -> f64 {
+    let temp: Result<f64, ParseFloatError> = arg.parse();
+
+    match temp {
+        Ok(value) => value,
+        Err(_msg) => {
+            println!("Invalid argument: {}\nPass in a number", arg);
+            exit(1)
+        }
+    }
+}
+
+fn convert(temp: f64, unit: Unit) -> f64 {
+    match unit {
+        Unit::Celsius => temp * (9f64 / 5f64) + 32f64,
+        Unit::Fahrenheit => (temp - 32f64) * (5f64 / 9f64),
+    }
+}
